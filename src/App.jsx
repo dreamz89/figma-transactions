964b5b4f7403f1cc8bf40b843daf6beefd2fb33e
Jocelyn Tan <jocelyntan89@hotmail.com>
2020-11-25 22:09:25 +0100

    fix transition

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [nav, setNav] = useState(1)
   const [accountNumber, setAccountNumber] = useState(null)
-  const [expanded, setExpanded] = useState(false)
+  const [overflow, setOverflow] = useState(false)
 
   const amounts = [45, 350, 120]
   const transactions = ['', '', '', '', '', '', '']
@@ -14,11 +14,13 @@ function App() {
   const handleExpansion = (index) => {
     if (accountNumber === null) {
       setAccountNumber(index)
-      setExpanded(true)
+      setOverflow(true)
       setNav(2)
     } else {
       setAccountNumber(null)
-      setExpanded(false)
+      setTimeout(() => { // set overflow hidden only after contraction is done
+        setOverflow(false)
+      }, 500)
       setNav(1)
     }
   }
@@ -31,7 +33,7 @@ function App() {
       </div>
       <h1>Morning Lee</h1>
       <h3 className="title-account">Your accounts</h3>
-      <div className={"accounts " + (expanded ? "expanded" : "")}>
+      <div className={"accounts " + (overflow ? "overflow" : "")}>
         {
           amounts.map((amount, index) => (
             <Account 
@@ -43,8 +45,8 @@ function App() {
           ))
         }
       </div>
-      <img src="add.svg" alt="add" className={"image-add " + (expanded ? "expanded" : "")} />
-      <div className={"transactions " + (expanded ? "expanded" : "")}>
+      <img src="add.svg" alt="add" className={"image-add " + (accountNumber !== null ? "expanded" : "")} />
+      <div className={"transactions " + (accountNumber !== null ? "expanded" : "")}>
         <h3 className="title-transactions">Transactions</h3>
         {
           transactions.map(() => <div class="row"></div>)
